b063147f2af97e58bec0cf9fbd87ba3fdef3efcc
bikeboi <michaelgithegi@gmail.com>
2021-07-29 19:30:11 +0300

    Updated README and configs

--- README.md
+++ README.md
@@ -2,7 +2,17 @@ Uniswap V2 Contracts
 ====================
 > Customized Uniswap V2 contract for me to tinker with
 
+This is mainly an exercise in getting better at writing
+(modern) Solidity through refactoring, and also understanding the Uniswap V2
+protocol.
+
 ## TODO
+### Admin tasks
 - [ ] Switch project to [hardhat](https://hardhat.org).
 - [ ] Upgrade contracts to Solidity ^0.8.0
-- [ ] Use OpenZeppelin library for ERC20 and ownership interfaces
+- [ ] Upgrade test scripts
+- [ ] Document new code
+
+### Code upgrades
+- [ ] Use OpenZeppelin library for ERC20 and ERC20Permit
+- [ ] Use `TransferHelper` library from Uniswap V3
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,17 @@
 import { task } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-ethers";
+import "hardhat-deploy";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+    const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    const bal = await account.getBalance();
-    console.log(account.address, ":", bal.toString());
-  }
+    for (const account of accounts) {
+        const bal = await account.getBalance();
+        console.log(account.address, ":", bal.toString());
+    }
 });
 
 // You need to export an object to set up your config
@@ -19,5 +21,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+    solidity: {
+        version: "0.8.4",
+        settings: {
+            optimizer: { enabled: true, runs: 1000 },
+        },
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+    },
 };
